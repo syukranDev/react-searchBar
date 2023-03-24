a32dd48b3d25b622d0cad1cb05074f66a158a6df
syukranDev <syukran.dev@gmail.com>
2023-03-24 23:17:39 +0800

    add more info in alert box

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -3,6 +3,6 @@ import './SearchResult.css'
 
 export const SearchResult = ({result}) => {
   return (
-    <div className='search-result' onClick={ e => alert(`You clicked on: ${result.name}`)}>{result.name}</div>
+    <div className='search-result' onClick={ e => alert(`You clicked on: ${result.name}!\n\nUser Infos:\nName: ${result.name}\nUsername: ${result.username}\nEmail: ${result.email}`)}>{result.name}</div>
   )
 }
